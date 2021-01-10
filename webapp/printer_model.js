2d9bbee564106653fba1deae3e6284241ddb4bca
y4cj4sul3 <hl6540@gmail.com>
2021-01-10 21:16:44 +0800

    Use bootstrap css

--- webapp/printer_model.js
+++ webapp/printer_model.js
@@ -5,7 +5,7 @@ class PrinterModel {
     this.node = template.cloneNode(true);
     this.div = this.node.querySelector(".printer_visualizer");
     this.div.id = printer_info["printer_name"];
-    document.body.append(this.node);
+    document.getElementById("printer_list").appendChild(this.node);
 
     // setup model
     this.printer_info = {};
@@ -31,15 +31,86 @@ class PrinterModel {
   }
 
   updateView() {
-    this.div.querySelector(".printer_name").innerHTML = this.printer_info["printer_name"];
-    this.div.querySelector(".printjob_name").innerHTML = this.printer_info["printjob_name"];
-    this.div.querySelector(".printer_state").innerHTML = this.printer_info["printer_state"];
-    this.div.querySelector(".printjob_state").innerHTML = this.printer_info["printjob_state"];
-    this.div.querySelector(".result").innerHTML = this.printer_info["result"];
+    this.div.querySelector(".printer_name").innerHTML = "UM" + this.printer_info["printer_name"];
 
-    this.div.querySelector(".input_img").src = this.printer_info["input_img_path"];
-    this.div.querySelector(".sim_img").src = this.printer_info["sim_img_path"];
-    this.div.querySelector(".predict_img").src = this.printer_info["predict_img_path"];
+    // printjob name
+    var printjob_text = this.div.querySelector(".printjob_name");
+    if (this.printer_info["printjob_name"] != null) {
+      if (this.printer_info["printer_state"] == "printing") {
+        printjob_text.innerHTML = "Print Job: " + this.printer_info["printjob_name"];
+      } else {
+        printjob_text.innerHTML = "Last Print Job: " + this.printer_info["printjob_name"];
+      }
+      printjob_text.style.display = "";
+    } else {
+      printjob_text.style.display = "none";
+    }
+
+    // printer state
+    var ps_span = this.div.querySelector(".printer_state");
+    ps_span.innerHTML = this.printer_info["printer_state"];
+    ps_span.className = "printer_state badge ";
+    switch (this.printer_info["printer_state"]) {
+      case "printing":
+        ps_span.className += "bg-primary";
+        break;
+      default:
+        // idle
+        ps_span.className += "bg-secondary";
+    }
+
+    // printjob state
+    var pj_span = this.div.querySelector(".printjob_state");
+    if (this.printer_info["printer_state"] === "printing" && this.printer_info["printjob_state"] != null) {
+      pj_span.style.display = "";
+      pj_span.innerHTML = this.printer_info["printjob_state"];
+      pj_span.className = "printjob_state badge ";
+      switch (this.printer_info["printjob_state"]) {
+        case "printing":
+          pj_span.className += "bg-primary";
+          break;
+        case "wait_cleanup":
+          pj_span.className += "bg-warning";
+          break;
+        default:
+          // preprint, post_print
+          pj_span.className += "bg-secondary";
+      }
+    } else {
+      pj_span.style.display = "none";
+    }
+
+    // result
+    var result_com = this.div.querySelector(".result");
+    if (this.printer_info["result"] != null) {
+      result_com.innerHTML = this.printer_info["result"];
+      result_com.style.display = "";
+    } else {
+      result_com.style.display = "none";
+    }
+
+    // images
+    var input_img = this.div.querySelector(".input_img");
+    if (this.printer_info["input_img_path"] != null) {
+      input_img.src = this.printer_info["input_img_path"];
+      input_img.style.display = "";
+    } else {
+      input_img.style.display = "none";
+    }
+    var sim_img = this.div.querySelector(".sim_img");
+    if (this.printer_info["sim_img_path"] != null) {
+      sim_img.src = this.printer_info["sim_img_path"];
+      sim_img.style.display = "";
+    } else {
+      sim_img.style.display = "none";
+    }
+    var predict_img = this.div.querySelector(".predict_img");
+    if (this.printer_info["predict_img_path"] != null) {
+      predict_img.src = this.printer_info["predict_img_path"];
+      predict_img.style.display = "";
+    } else {
+      predict_img.style.display = "none";
+    }
   }
 
   expired() {
